5b279d80830a0e45ef8e89dfbd02311da1d6e80e
Chet Wilk <chetw.email@gmail.com>
2017-03-14 00:18:29 -0400

    Inital commit

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+// #include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // printf("bool is %lu\n", sizeof(bool)); // cs50.h
+    // printf("string is %lu\n", sizeof(string)); // cs50.h
+    printf("char is %lu\n", sizeof(char));
+    printf("int is %lu\n", sizeof(int));
+    printf("float is %lu\n", sizeof(float));
+    printf("double is %lu\n", sizeof(double));
+    printf("long long is %lu\n", sizeof(long long));
+}
